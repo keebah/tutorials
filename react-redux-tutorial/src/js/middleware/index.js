b7dd680ab4ffbbf737f66447809e1d82735616f1
Konrad <konrad.woehler@gmail.com>
2020-11-22 12:52:59 +0100

    add app, a form which let's me add articles to the store

---
+++ react-redux-tutorial/src/js/middleware/index.js
@@ -0,0 +1,35 @@
+import { ADD_ACTION, ADD_ARTICLE } from "../constants/action-types";
+
+const forbiddenWords = ["spam", "money"]
+
+export function forbiddenWordsMiddleware( {dispatch }) {
+    return function(next) {
+        return function(action) {
+            // do something
+            if (action.type === ADD_ARTICLE) {
+                const foundWord = forbiddenWords.filter(word => action.payload.title.includes(word));
+                if (foundWord.length) {
+                    return dispatch({ type: "FOUND_BAD_WORD" });
+                }
+            }
+            return next(action);
+        }
+    }
+}
+const forbiddenWords2 = ["test", "money"]
+
+export function forbiddenWordsMiddleware2( {dispatch }) {
+    return function(next) {
+        return function(action) {
+            // do something
+            if (action.type === ADD_ARTICLE) {
+                const foundWord = forbiddenWords2.filter(word => action.payload.title.includes(word));
+                if (foundWord.length) {
+                    return dispatch({ type: "FOUND_BAD_WORD" });
+                }
+            }
+            return next(action);
+        }
+    }
+}
+
--- react-redux-tutorial/src/js/store/index.js
+++ react-redux-tutorial/src/js/store/index.js
@@ -1,6 +1,7 @@
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
 import rootReducer from "../reducers/index";
+import { forbiddenWordsMiddleware, forbiddenWordsMiddleware2 } from "../middleware";
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(forbiddenWordsMiddleware, forbiddenWordsMiddleware2));
 
 export default store;
